d42d4b60f419acb520c106a54a7ca376ecfcee5a
Khawaja Muhammad Ali <khawajaali246@gmail.com>
2019-07-18 21:03:29 +0500

    backend of users register and login routes done and tested

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb+srv://Ali:Happy@jobportal-q2k0h.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "mongodb://Ali:hey@jobportal-shard-00-00-q2k0h.mongodb.net:27017,jobportal-shard-00-01-q2k0h.mongodb.net:27017,jobportal-shard-00-02-q2k0h.mongodb.net:27017/test?ssl=true&replicaSet=jobportal-shard-0&authSource=admin&retryWrites=true&w=majority"
 };
